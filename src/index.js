943cdfe9fb3359fe2ecbc8118788dcecdcdcd275
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-04-24 17:23:45 -0400

    Implement functionality that will display welcome message for user

--- src/index.js
+++ src/index.js
@@ -3,25 +3,48 @@ import './css/base.scss';
 //import './dataHandler.js';
 
 import User from './user.js'
+import TripsRepo from './tripsRepo.js'
 import dataHandler from './dataHandler.js';
+import domUpdates from './domUpdates.js'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 
-//getting promise
-console.log("First", dataHandler.getAllTravelers())
+let welcome = document.querySelector('#welcome')
+
+window.onload = onStartup();
+
+function onStartup() {
+  let user = {}
+  dataHandler.getSingleTraveler(2)//have to pass in argument based on login in iteration3
+    .then(result => {
+        user = new User(result);
+      })
+    .then(() => dataHandler.getAllTrips())
+    .then(trips => {
+            //let tripsRepo = new TripsRepo(trips)
+            //tripsRepo.filterUserTrips(user.id)
+            console.log("BOOYA",trips)
+            user.returnUsersTrips(trips);
+            console.log(user);
+            domUpdates.welcomeMessage(user.name)
+          })
+}
 
 //just getting the data in the promise
 dataHandler.getAllTravelers().then((result) => {
-//doing something with the data
-  console.log("ALL TRAVELERS",result)
-//  randomClass.method(result)
+  //doing something with the data
+  //console.log("ALL TRAVELERS",result)
+  //  randomClass.method(result)
 })
 
 //making sure getSingleTraveler works
 dataHandler.getSingleTraveler(1).then((result) => {
-  console.log("SINGLE TRAVELER", result.name)
+  //console.log("SINGLE TRAVELER", result.name)
+  let user = new User(result);
+  console.log(user);
 })
 
 
 dataHandler.getAllTrips().then((result) => {
-  console.log("ALL TRIPS", result)
+  let tripsRepo = new TripsRepo(result);
+  console.log(tripsRepo);
 })

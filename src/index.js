2e4352ec61fce9889a18a59f7f9accfda34b8d2d
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-04-26 17:36:03 -0400

    Remove comments & console.logs

--- src/index.js
+++ src/index.js
@@ -1,50 +1,27 @@
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-//import './dataHandler.js';
 
 import User from './user.js'
-import TripsRepo from './tripsRepo.js'
 import dataHandler from './dataHandler.js';
 import domUpdates from './domUpdates.js'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 
-let welcome = document.querySelector('#welcome')
+let welcome = document.querySelector('#welcome');
+
+let user;
 
 window.onload = onStartup();
 
-function onStartup() {
-  let user = {}
-  dataHandler.getSingleTraveler(2)//have to pass in argument based on login in iteration3
+function onStartup() { //need parameter that will represent user
+  dataHandler.getSingleTraveler(30)//have to pass in argument based on login in iteration3
     .then(result => {
         user = new User(result);
       })
     .then(() => dataHandler.getAllTrips())
     .then(trips => {
-            //let tripsRepo = new TripsRepo(trips)
-            //tripsRepo.filterUserTrips(user.id)
-            console.log("BOOYA",trips)
             user.returnUsersTrips(trips);
-            console.log(user);
-            domUpdates.welcomeMessage(user.name)
-          })
+            user.returnTotalSpent(2020);
+            //domUpdates.welcomeUser(this.name, this.total);
+            //domUpdates.populateCards(user.trips)
+//************ WHAT IS GOING ON WITH THIS CONSOLE.LOG???*****************
+}).then(() => console.log(user, "\n", "user.total:", user.total))
 }
-
-//just getting the data in the promise
-dataHandler.getAllTravelers().then((result) => {
-  //doing something with the data
-  //console.log("ALL TRAVELERS",result)
-  //  randomClass.method(result)
-})
-
-//making sure getSingleTraveler works
-dataHandler.getSingleTraveler(1).then((result) => {
-  //console.log("SINGLE TRAVELER", result.name)
-  let user = new User(result);
-  console.log(user);
-})
-
-
-dataHandler.getAllTrips().then((result) => {
-  let tripsRepo = new TripsRepo(result);
-  console.log(tripsRepo);
-})
